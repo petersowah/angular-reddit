0fac3c0519ec57c5bc08716f4eb19605f4a81ee9
Peter Sowah <petersowah@gmail.com>
2017-07-18 22:39:35 +0000

    complete project

---
+++ src/app/article/article.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit, HostBinding, Input } from '@angular/core';
+import { Article } from './article.model';
+
+@Component({
+  selector: 'app-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.css']
+})
+export class ArticleComponent implements OnInit {
+
+  @HostBinding('attr.class') cssClass = 'row';
+  @Input() article: Article;
+
+  constructor() {
+    // article is getting populated by Input decorator, no need for a constructor now.
+  }
+
+  voteUp(): boolean {
+    this.article.voteUp();
+    return false;
+  }
+
+  voteDown(): boolean {
+    this.article.voteDown();
+    return false;
+  }
+
+  ngOnInit() {
+  }
+}
